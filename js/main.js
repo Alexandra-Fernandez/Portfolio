86142ddab34ca9294404fc8c4a5ebbb1be273cf4
Alexandra <alexandrafernandezquispe@gmail.com>
2016-12-01 16:39:42 -0500

    foto

--- js/main.js
+++ js/main.js
@@ -4,20 +4,12 @@ $(document).ready(function(){
     }) 
     
     $("#newhome").click(function(){
-          // $(".contenido").fadeOut("slow");
         $("#home").slideToggle(0.5,function(){$(".contenido").hide()})
-          // $("#home").slideDown("slow",function(){$(".contenido").hide()})
     })
 
     $("i.fa.fa-bars").on("click",function(){$("aside ul").toggleClass("mostrar")})
   
 })
-  
-
-
-
-
-
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -34,22 +26,3 @@ $(function() {
   });
 });
 
-
-
-//  $(".fa.fa-github").click(function(){
-//         $(".fa.fa-html5").css({"left": "12px", "top": "23px"});
-//         $(".jade").css({"top": "-13px", "left": "138px"});
-//         $(".fa.fa-css3").css({"right": "26px", "top": "23px"});
-// })
-
-
-// $(document).ready(function(){
- 
-//   $("ul li:nth-child(2)").on("click",function(){$(".about").slideDown("slow")});//no corre con id?
-//   $("ul li:nth-child(3)").on("click",function(){$(".portfolio").slideDown("slow")});
-  
-//   })
-
-
-// $("ul li:nth-child(2)").on("click",function(){$("#about").show().addClass("animated slideInDown")});
-//   $("ul li:nth-child(3)").on("click",function(){$("#portfolio").show().addClass("animated slideInDown")});
